f1254589fa196e5cead24c1326b779b80f5c93e0
tuomohy <tuomo.hyttinen@kapsi.fi>
2012-05-15 22:10:51 +0300

    added camera-placeholder

---
+++ leogame/src/main/java/fi/leoteam/leogame/rendering/Camera.java
@@ -0,0 +1,9 @@
+package fi.leoteam.leogame.rendering;
+
+public class Camera {
+	
+	private float x;
+	private float y;
+	
+
+}
--- leogame/src/main/java/fi/leoteam/leogame/rendering/IsometricRenderer.java
+++ leogame/src/main/java/fi/leoteam/leogame/rendering/IsometricRenderer.java
@@ -5,11 +5,7 @@ import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
 import static org.lwjgl.opengl.GL11.GL_QUADS;
-import static org.lwjgl.opengl.GL11.GL_REPEAT;
 import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_S;
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_T;
 import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glBindTexture;
 import static org.lwjgl.opengl.GL11.glBlendFunc;
@@ -17,12 +13,13 @@ import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+import static org.lwjgl.opengl.GL11.glVertex2f;
 
 import org.apache.log4j.Logger;
-import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
-import org.lwjgl.util.Color;
 
 public class IsometricRenderer {
 	
